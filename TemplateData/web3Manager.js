0100bdf7473b3e9a0a0c9a33847fce812e290866
GothTokenNetwork <gothtoken@gmail.com>
2022-05-27 08:31:16 +0100

    Update web3Manager.js

--- TemplateData/web3Manager.js
+++ TemplateData/web3Manager.js
@@ -272,9 +272,9 @@ async function enterPool (source, amount)
             const feeMod = await stakeContract.methods.enterFeeMod().call();
             const fee = amount / feeMod;
 
-            console.log(web3.utils.toWei(fee.toString()));
+            console.log(web3.utils.toWei(fee.toFixed(18)));
 
-            await stakeContract.methods.enter(web3.utils.toWei(amount)).send({value: web3.utils.toWei(fee.toString()), from: window.ethereum.selectedAddress});
+            await stakeContract.methods.enter(web3.utils.toWei(amount)).send({value: web3.utils.toWei(fee.toFixed(18)), from: window.ethereum.selectedAddress});
             window.unityInstance.SendMessage(source, "OnEnter");
         }
         catch(err)
